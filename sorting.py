b232807c05f62de9561660d3e309225ce7891eea
Mateusz Owsianka <mowsianka13@gmail.com>
2020-09-14 22:34:41 +0200

    Added information about what app should does and added few first lines.

---
+++ sorting.py
@@ -0,0 +1,10 @@
+#Sorting app
+#First user provide numbers for sort (or generator do this)
+#Secondly user choosing one of the sort method
+#Finally we obtain the result
+#User can use another method or provide new numbers
+
+print("Sorting app.")
+print("What do you want to do?")
+print("1. Enter my values (please use comma as separator")
+print("2. Generate random numbers")
