e1c99927ddfc29d33be898d13ea91047ff4ce187
Mateusz Owsianka <mowsianka13@gmail.com>
2020-09-16 12:37:50 +0200

    Add prototype of main and submain loops.

--- sorting.py
+++ sorting.py
@@ -4,7 +4,28 @@
 #Finally we obtain the result
 #User can use another method or provide new numbers
 
-print("Sorting app.")
-print("What do you want to do?")
-print("1. Enter my values (please use comma as separator")
-print("2. Generate random numbers")
+#Starting conditions
+prog_set = True
+
+while prog_set:
+    print("""Sorting app.\n
+        What do you want to do?\n
+        1. Enter my values (please use comma as separator)\n
+        2. Generate random numbers\n
+        3. Exit""")
+
+    remote_control = input()
+    
+    if remote_control = "1":
+        pass
+    elif remote_control = "2":
+        pass
+    elif remote_control = "3":
+        prog_set = False
+    else:
+        pass
+    while 1:
+        print("""Choose the sort method:\n
+            1. Bubble sort
+            """)
+        pass
