cad325e79f12fb1382b744986311ac90463c20a2
Mateusz Owsianka <mowsianka13@gmail.com>
2020-09-20 11:48:01 +0200

    Add conditionals in subloop.

--- sorting.py
+++ sorting.py
@@ -10,6 +10,7 @@ import insert
 prog_set = True
 
 while prog_set:
+    subprog_set = True
     print("""Sorting app.\n
         What do you want to do?\n
         1. Enter my values (please use comma as separator)\n
@@ -26,9 +27,24 @@ while prog_set:
         prog_set = False
     else:
         pass
-    while 1:
+    while subprog_set:
         print("""Choose the sort method:\n
             1. Bubble sort\n
             2. Insert sort\n
+            3. Quick sort\n
+            4. Exit to menu\n
             """)
-        pass
+        mini_remote_control = input()
+        if mini_remote_control == "4":
+            subprog_set = False
+        elif mini_remote_control == "1":
+            #bubbleSort()
+            pass
+        elif mini_remote_control == "2":
+            #insertSort()
+            pass
+        elif mini_remote_control == "3":
+            #quickSort()
+            pass
+        else:
+            pass
